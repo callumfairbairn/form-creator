8ffbd3a3bfb3e1d424917fe25af739576b418343
Callum Fairbairn <86301717+callum-fairbairn-cazoo@users.noreply.github.com>
2022-07-22 14:52:04 +0100

    Add Create and Preview components

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import App from './App';
 import { renderWithRouter } from "./testHelpers";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
-  renderWithRouter(<App />);
+describe("App", () => {
+  it("renders the Create page", () => {
+    renderWithRouter(<App />);
 
-  expect(screen.getByText("Create")).toBeInTheDocument();
-  expect(screen.getByText("Preview")).toBeInTheDocument();
-});
+    userEvent.click(screen.getByText("Create"))
+
+    expect(screen.getByText("Create your form")).toBeInTheDocument();
+  })
+
+  it("renders the Preview page", () => {
+    renderWithRouter(<App />);
+
+    userEvent.click(screen.getByText("Preview"))
+
+    expect(screen.getByText("Preview your form")).toBeInTheDocument();
+  })
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Navigate } from "./components/Navigate";
+import { Route, Routes } from "react-router-dom";
+import { Create } from "./pages/Create";
+import { Preview } from "./pages/Preview";
 
 function App() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-zinc-400 p-5">
       <div className="container bg-base-100 mx-auto p-4">
         <Navigate>
-          Sup
+          <Routes>
+            <Route path="/create" element={<Create />} />
+            <Route path="/preview" element={<Preview />} />
+            <Route path="*" element={<Create />} />
+          </Routes>
         </Navigate>
       </div>
     </div>
